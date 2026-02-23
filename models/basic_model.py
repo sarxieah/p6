69a5e2f5de2b8021a06d7598098a4c3f2d921ccc
sarahwxie <sarahwxie@gmail.com>
2026-02-24 07:44:44 +0800

    first commit

---
+++ models/basic_model.py
@@ -0,0 +1,28 @@
+from models.model import Model
+from tensorflow.keras import Sequential, layers
+from tensorflow.keras.layers import Rescaling
+from tensorflow.keras.optimizers import RMSprop, Adam
+
+class BasicModel(Model):
+    def _define_model(self, input_shape, categories_count):
+        self.model = Sequential([
+            Rescaling(1.0 / 255, input_shape=input_shape),
+            layers.Conv2D(16, (3, 3), activation='relu'),
+            layers.MaxPooling2D((2, 2)),
+            layers.Conv2D(32, (3, 3), activation='relu'),
+            layers.MaxPooling2D((2, 2)),
+            layers.Conv2D(64, (3, 3), activation='relu'),
+            layers.MaxPooling2D((2, 2)),
+            layers.Conv2D(64, (3, 3), activation='relu'),
+            layers.MaxPooling2D((2, 2)),
+            layers.Flatten(),
+            layers.Dense(24, activation='relu'),
+            layers.Dense(categories_count, activation='softmax'),
+        ])
+    
+    def _compile_model(self):
+        self.model.compile(
+            optimizer=RMSprop(learning_rate=0.001),
+            loss='categorical_crossentropy',
+            metrics=['accuracy'],
+        )
---
+++ player.py
@@ -0,0 +1,133 @@
+from config import BOARD_SIZE, categories, image_size
+from tensorflow.keras import models
+import numpy as np
+import tensorflow as tf
+import os
+from glob import glob
+
+class TicTacToePlayer:
+    def get_move(self, board_state):
+        raise NotImplementedError()
+
+class UserInputPlayer:
+    def get_move(self, board_state):
+        inp = input('Enter x y:')
+        try:
+            x, y = inp.split()
+            x, y = int(x), int(y)
+            return x, y
+        except Exception:
+            return None
+
+import random
+
+class RandomPlayer:
+    def get_move(self, board_state):
+        positions = []
+        for i in range(BOARD_SIZE):
+            for j in range(BOARD_SIZE):
+                if board_state[i][j] is None:
+                    positions.append((i, j))
+        return random.choice(positions)
+
+from matplotlib import pyplot as plt
+from matplotlib.image import imread
+import cv2
+
+class UserWebcamPlayer:
+    def __init__(self):
+        self._emotion_model = None
+
+    def _process_frame(self, frame):
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        width, height = frame.shape
+        size = min(width, height)
+        pad = int((width-size)/2), int((height-size)/2)
+        frame = frame[pad[0]:pad[0]+size, pad[1]:pad[1]+size]
+        return frame
+
+    def _load_emotion_model(self):
+        if self._emotion_model is not None:
+            return self._emotion_model
+
+        model_paths = sorted(glob(os.path.join('results', '*.keras')))
+        if not model_paths:
+            raise FileNotFoundError("No .keras model found in results/. Train and save a model first.")
+        self._emotion_model = models.load_model(model_paths[-1])
+        return self._emotion_model
+
+    def _access_webcam(self):
+        import cv2
+        cv2.namedWindow("preview")
+        vc = cv2.VideoCapture(0)
+        if vc.isOpened(): # try to get the first frame
+            rval, frame = vc.read()
+            frame = self._process_frame(frame)
+        else:
+            rval = False
+            frame = None
+        while rval:
+            cv2.imshow("preview", frame)
+            rval, frame = vc.read()
+            frame = self._process_frame(frame)
+            key = cv2.waitKey(20)
+            # macOS keycodes can differ; support common confirm/exit keys.
+            if key in (13, 10, 32, 27):  # Enter/Return/Space/Esc
+                break
+
+        vc.release()
+        cv2.destroyWindow("preview")
+        if frame is None:
+            raise RuntimeError("Could not read a frame from webcam.")
+        return frame
+
+    def _print_reference(self, row_or_col):
+        print('reference:')
+        for i, emotion in enumerate(categories):
+            print('{} {} is {}.'.format(row_or_col, i, emotion))
+    
+    def _get_row_or_col_by_text(self):
+        try:
+            val = int(input())
+            return val
+        except Exception as e:
+            print('Invalid position')
+            return None
+    
+    def _get_row_or_col(self, is_row):
+        try:
+            row_or_col = 'row' if is_row else 'col'
+            self._print_reference(row_or_col)
+            img = self._access_webcam()
+            emotion = self._get_emotion(img)
+            if type(emotion) is not int or emotion not in range(len(categories)):
+                print('Invalid emotion number {}'.format(emotion))
+                return None
+            print('Emotion detected as {} ({} {}). Enter \'text\' to use text input instead (0, 1 or 2). Otherwise, press Enter to continue.'.format(categories[emotion], row_or_col, emotion))
+            inp = input()
+            if inp == 'text':
+                return self._get_row_or_col_by_text()
+            return emotion
+        except Exception as e:
+            # error accessing the webcam, or processing the image
+            raise e
+    
+    def _get_emotion(self, img) -> int:
+        model = self._load_emotion_model()
+
+        img = tf.convert_to_tensor(img, dtype=tf.float32)
+        img = tf.expand_dims(img, axis=-1)  # (N, N, 1)
+        img = tf.image.resize(img, image_size)  # (H, W, 1)
+        img = tf.image.grayscale_to_rgb(img)  # (H, W, 3)
+        img = tf.expand_dims(img, axis=0)  # (1, H, W, 3)
+
+        prediction = model.predict(img, verbose=0)
+        return int(np.argmax(prediction, axis=-1)[0])
+    
+    def get_move(self, board_state):
+        row, col = None, None
+        while row is None:
+            row = self._get_row_or_col(True)
+        while col is None:
+            col = self._get_row_or_col(False)
+        return row, col
